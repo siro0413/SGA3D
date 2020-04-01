c5f68fb088072f258c3023d18b4646b4bde16ffd
siro0413 <48557104+siro0413@users.noreply.github.com>
2020-04-01 17:43:31 +0900

    Add files via upload
    Brush Type 
    - Circle Type Add
    - Noise Type Add
    - Flat Type Add

---
+++ Assignment/TerrainBrush_01/Terrain.h
@@ -0,0 +1,55 @@
+#pragma once
+
+class Terrain : public Renderer
+{
+public:
+	typedef VertexTextureNormal TerrainVertex;
+
+public:
+	Terrain(Shader* shader, wstring heightFile);
+	~Terrain();
+
+	void Update();
+	void Render();
+
+	void BaseMap(wstring file);
+
+	float GetHeight(Vector3& position);
+	float GetHeightPick(Vector3& position);
+	Vector3 GetPickedPosition(); //Raycast
+
+private:
+	void CreateVertexData();
+	void CreateIndexData();
+	void CreateNormalData();
+	void RaiseHeight(Vector3& pos,UINT type, UINT range);
+
+private:	
+	Texture* heightMap;
+
+	UINT width = 128;
+	UINT height = 128;
+
+	bool Rect = false;
+	bool Sphere = false;
+	bool Flat = false;
+	bool Noise = false;
+	TerrainVertex* vertices;
+
+	UINT* indices;
+
+	Texture* baseMap = NULL;
+	ID3DX11EffectShaderResourceVariable* sBaseMap;
+
+	struct BushDesc
+	{
+		Color Color = D3DXCOLOR(0, 1, 0, 1);
+		Vector3 Location;
+		UINT Type = 0;
+		UINT Range = 1;
+		float Padding[3];
+	}brushDesc;
+
+	ConstantBuffer* brushBuffer;
+	ID3DX11EffectConstantBuffer* sBrushBuffer;
+};
